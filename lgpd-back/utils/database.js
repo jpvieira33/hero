f9a43e1abb0f6935366e19451b6bdfa904fcb67d
jpvieira33 <jpvieira33@hotmail.com>
2024-03-16 13:10:29 -0300

    alterações no backend

--- lgpd-back/utils/database.js
+++ lgpd-back/utils/database.js
@@ -1,8 +1,13 @@
-const { Sequelize } = require('sequelize');
-const path = require('path');
+import Sequelize from 'sequelize';
+import env from 'process'
+import path from 'path';
 
-const env = process.env.NODE_ENV || 'development';
-const config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
+import configJson from '../config/config.json' assert { type: 'json' };
+const config = configJson[env] || configJson['development']; 
+
+
+//const env = process.env.NODE_ENV || 'development';
+//const config = path.join(__dirname, '..', 'config', 'config.json')[env];
 
 const sequelize = new Sequelize(
     config.database, 
